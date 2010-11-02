cae080e6018c9def7b8a52dd732c84b2826c6801
Josh Lindsey <josh@cloudspace.com>
2010-11-02 11:31:06 -0400

    Complete Rutty::Runner test

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -1,7 +1,26 @@
 require 'helper'
 
 class TestRunner < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "A Runner instance" do
+    setup { 
+      @r = Rutty::Runner.new 
+      %x(#{File.join File.dirname(__FILE__), '..', 'bin', 'rutty'} init)
+    }
+    
+    should "respond to config" do
+      assert_respond_to @r, :config
+    end
+    
+    should "respond to nodes" do
+      assert_respond_to @r, :nodes
+    end
+    
+    should "return the correct object type for #config" do
+      assert_instance_of Rutty::Config, @r.config
+    end
+    
+    should "return the correct object type for #nodes" do
+      assert_instance_of Rutty::Nodes, @r.nodes
+    end
   end
 end

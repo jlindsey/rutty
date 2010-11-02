ab4e195c877f90fe518f93138d6313afe3573865
Josh Lindsey <josh@cloudspace.com>
2010-11-02 10:46:54 -0400

    Rename test_rutty to test_runner

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class TestRutty < Test::Unit::TestCase
+class TestRunner < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end

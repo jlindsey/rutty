a93b5af5e405d10fe979275603fcbdf653fdd2a2
Josh Lindsey <josh@cloudspace.com>
2010-11-02 11:50:11 -0400

    Add Rutty::Consts tests

---
+++ test/test_rutty_consts.rb
@@ -0,0 +1,17 @@
+require 'helper'
+
+class TestRuttyConsts < Test::Unit::TestCase
+  context "The Rutty::Consts module" do
+    should "have all the constants set" do
+      assert_not_nil Rutty::Consts::CONF_DIR
+      assert_not_nil Rutty::Consts::GENERAL_CONF
+      assert_not_nil Rutty::Consts::NODES_CONF
+    end
+    
+    should "have the constants be the proper values" do
+      assert_equal Rutty::Consts::CONF_DIR, File.join(ENV['HOME'], '.rutty')
+      assert_equal Rutty::Consts::GENERAL_CONF, File.join(Rutty::Consts::CONF_DIR, 'defaults.yaml')
+      assert_equal Rutty::Consts::NODES_CONF, File.join(Rutty::Consts::CONF_DIR, 'nodes.yaml')
+    end
+  end
+end

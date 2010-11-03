2df84165bb5df17b636cafd7dbbdde75f1259bf3
Josh Lindsey <josh@cloudspace.com>
2010-11-03 16:46:08 -0400

    Add new helper methods for ensuring idempotency on tests that require reading from the configs

--- test/helper.rb
+++ test/helper.rb
@@ -7,7 +7,28 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rutty'
 
 class Test::Unit::TestCase
+  TEST_CONF_DIR = File.join(File.dirname(__FILE__), '..', 'tmp')
+  TEST_GENERAL_CONF = File.join(TEST_CONF_DIR, 'defaults.yaml')
+  TEST_NODES_CONF = File.join(TEST_CONF_DIR, 'nodes.yaml')
+  
+  RUTTY_BIN = File.join File.dirname(__FILE__), '..', 'bin', 'rutty'
+  
   def call_init
-    %x(#{File.join File.dirname(__FILE__), '..', 'bin', 'rutty'} init)
+    unless File.exists? TEST_CONF_DIR
+      %x(#{RUTTY_BIN} init #{TEST_CONF_DIR})
+    end
+  end
+  
+  def clean_test_config
+    %x(rm -rf #{TEST_CONF_DIR}) if File.exists? TEST_CONF_DIR
+  end
+  
+  def seed_nodes
+    %x(#{RUTTY_BIN} add_node localhost -c #{TEST_CONF_DIR} -k ~/.ssh/id_rsa.pem -u root --tags localhost,test -p 22)
+  end
+  
+  def ensure_fresh_config!
+    clean_test_config
+    call_init
   end
 end

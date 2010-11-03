222b266fa6e60aa6856a02655218bee1b3068ea9
Josh Lindsey <josh@cloudspace.com>
2010-11-03 12:23:06 -0400

    Add Rutty::Nodes test file

---
+++ test/test_nodes.rb
@@ -0,0 +1,19 @@
+require 'helper'
+
+class TestNodes < Test::Unit::TestCase
+  context "The Rutty::Nodes class" do
+    setup do 
+      call_init
+    end
+    
+    teardown do
+      
+    end
+    
+    should "load the nodes from the config file"
+  end
+  
+  context "A Rutty::Nodes instance" do
+    
+  end
+end

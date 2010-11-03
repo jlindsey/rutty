700ee54cbe52c722cc01e0971c84ee8f4fc57325
Josh Lindsey <josh@cloudspace.com>
2010-11-03 16:48:57 -0400

    Rewrite much of the Rutty::Helpers tests to reflect changes

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -3,33 +3,27 @@ require 'helper'
 class TestHelpers < Test::Unit::TestCase
   context "The Rutty::Helpers module" do
     setup do
-      class Testing
-        include Rutty::Helpers
-      end
-      
-      @obj = Testing.new
+      ensure_fresh_config!
+      @obj = Rutty::Runner.new TEST_CONF_DIR
     end
     
     should "mixin its helper methods" do
       assert_respond_to @obj, :check_installed!
-      assert_respond_to @obj, :get_version
+    end
+    
+    should "respond to its static methods" do
+      assert_respond_to Rutty::Helpers, :get_version
     end
     
     should "get the correct version" do
-      assert_equal @obj.get_version, Rutty::Version::STRING
+      assert_equal Rutty::Helpers.get_version, Rutty::Version::STRING
     end
     
     should "correctly check the installation status" do
-      if File.exists? Rutty::Consts::CONF_DIR
-        %x(mv #{Rutty::Consts::CONF_DIR} #{Rutty::Consts::CONF_DIR}.bak)
-      end
+      clean_test_config
       
       assert_raise Rutty::NotInstalledError do 
         @obj.check_installed!
-      end
-      
-      if File.exists? "#{Rutty::Consts::CONF_DIR}.bak"
-        %x(mv #{Rutty::Consts::CONF_DIR}.bak #{Rutty::Consts::CONF_DIR})
       end
     end
   end

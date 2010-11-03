fac738011b501562dc4823b3888f200e4e056572
Josh Lindsey <josh@cloudspace.com>
2010-11-03 16:50:17 -0400

    Update Rutty::Runner tests to reflect changes

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -2,17 +2,16 @@ require 'helper'
 
 class TestRunner < Test::Unit::TestCase
   context "A Runner instance" do
-    setup { 
-      @r = Rutty::Runner.new 
-      call_init
-    }
-    
-    should "respond to config" do
-      assert_respond_to @r, :config
+    setup do
+      ensure_fresh_config!
+      @r = Rutty::Runner.new TEST_CONF_DIR
     end
     
-    should "respond to nodes" do
+    should "respond to its methods" do
+      assert_respond_to @r, :config
       assert_respond_to @r, :nodes
+      assert_respond_to @r, :config_dir
+      assert_respond_to @r, :config_dir=
     end
     
     should "return the correct object type for #config" do
@@ -21,6 +20,15 @@ class TestRunner < Test::Unit::TestCase
     
     should "return the correct object type for #nodes" do
       assert_instance_of Rutty::Nodes, @r.nodes
+    end
+    
+    should "correctly initialize with a supplied configuration directory" do
+      assert_equal @r.config_dir, TEST_CONF_DIR
+    end
+    
+    should "correctly fall back to default configuration directory" do
+      r = Rutty::Runner.new
+      assert_equal r.config_dir, Rutty::Consts::CONF_DIR
     end
   end
 end

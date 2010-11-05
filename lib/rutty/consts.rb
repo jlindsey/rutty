9b2a2af1eaa58e99e31dcf65612a706fab3e4d3c
Josh Lindsey <josh@cloudspace.com>
2010-11-05 14:07:28 -0400

    Add docs for Rutty::Consts

--- lib/rutty/consts.rb
+++ lib/rutty/consts.rb
@@ -1,10 +1,20 @@
 module Rutty
+  ##
+  # Simple container module for constants.
+  #
+  # @author Josh Lindsey
+  # @since 2.0.0
   module Consts
+    ## Name of the general (defaults) config file
     GENERAL_CONF_FILE = 'defaults.yaml'
+    ## Name of the datastore file for user-defined nodes
     NODES_CONF_FILE = 'nodes.yaml'
     
+    ## Default configuaration storage directory
     CONF_DIR = File.join(ENV['HOME'], '.rutty')
+    ## Default general (defaults) config file location
     GENERAL_CONF = File.join(CONF_DIR, GENERAL_CONF_FILE)
+    ## Default nodes datastore file location
     NODES_CONF = File.join(CONF_DIR, NODES_CONF_FILE)
   end
 end

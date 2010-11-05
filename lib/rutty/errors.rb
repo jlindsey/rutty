c7ea843a3a45a1a950a48ec89db5248265ed4948
Josh Lindsey <josh@cloudspace.com>
2010-11-05 14:04:42 -0400

    Add docs for error classes

--- lib/rutty/errors.rb
+++ lib/rutty/errors.rb
@@ -1,5 +1,15 @@
 module Rutty
+  ##
+  # Raised by {Rutty::Helpers#check_installed!} if the check fails.
+  #
+  # @author Josh Lindsey
+  # @since 2.0.0
   class NotInstalledError < StandardError; end
   
+  ##
+  # Raised by various {Rutty::Actions} methods on invalid options, etc.
+  #
+  # @author Josh Lindsey
+  # @since 2.0.0
   class BadUsage < StandardError; end
 end

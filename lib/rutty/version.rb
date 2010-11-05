63c0e1eb7f60bdd738e3fdca8a9b76f4311c4283
Josh Lindsey <josh@cloudspace.com>
2010-11-05 12:36:36 -0400

    Add Rutty::Version docs

--- lib/rutty/version.rb
+++ lib/rutty/version.rb
@@ -1,4 +1,11 @@
 module Rutty
+  
+  ##
+  # The current version of the gem, expressed in Ruby code so it can
+  # be accessed programmatically.
+  #
+  # @author Josh Lindsey
+  # @see http://semver.org
   module Version
     MAJOR = 2
     MINOR = 1

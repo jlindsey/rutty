a2cd72136b2bbd43f9dc69361872949cc248a5d6
Josh Lindsey <josh@cloudspace.com>
2010-11-05 13:54:33 -0400

    Add Rutty::Node docs

--- lib/rutty/node.rb
+++ lib/rutty/node.rb
@@ -1,20 +1,51 @@
 require 'rutty/config'
 
 module Rutty
-  class Node < Config    
+  
+  ##
+  # A wrapper class representing an individual node. Normally contained by {Rutty::Nodes}.
+  #
+  # @see Rutty::Config
+  # @author Josh Lindsey
+  # @since 2.0.0
+  class Node < Config
+    ##
+    # Initialize a new {Rutty::Node} instance by merging the user-provided data with
+    # the configured defaults.
+    #
+    # @see Rutty::Config#initialize
+    # @param [Hash] data The data provided by the user
+    # @param [Hash] defaults The defaults data provided by the {Rutty::Config} class
     def initialize data, defaults = {}
       merged_data = defaults.merge data
       super merged_data
     end
     
+    ##
+    # Whether this object's tags array includes the specified tag.
+    #
+    # @param [String] tag The tag string to check for
     def has_tag? tag
       self.tags.include? tag
     end
     
+    ##
+    # Relation of this {Rutty::Node} to another {Rutty::Node}. Used for sorting.
+    # Compares the host property of the nodes, as it's the only property guaranteed to be
+    # set and unique.
+    #
+    # @see http://ruby-doc.org/core/classes/String.html#M000763
+    # @param [Rutty::Node] other The other {Rutty::Node} to compare to
+    # @return [Integer] Relation of self to other: -1 for less-than, 0 for equal-to, 1 for greater-than
     def <=> other
       self.host <=> other.host
     end
     
+    ##
+    # Checks for object eqality. Checks each property of this {Node} against the other.
+    #
+    # @param (see Rutty::Node#<=>)
+    # @return [Boolean]
     def == other
       self.host == other.host and
       self.port == other.port and

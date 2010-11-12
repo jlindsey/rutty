71a1da9b24c13777bedd11bd263236ab9cfe907b
Josh Lindsey <josh@cloudspace.com>
2010-11-12 16:28:17 -0500

    Add output_format methods to Rutty::Runner

--- lib/rutty.rb
+++ lib/rutty.rb
@@ -22,6 +22,9 @@ module Rutty
   # @since 2.0.0 
   class Runner
     attr_writer :config_dir
+    attr :config
+    attr :nodes
+    attr :output_format
   
     include Rutty::Consts
     include Rutty::Helpers
@@ -39,7 +42,7 @@ module Rutty
     # Lazy-load the {Rutty::Config} object for this instance, based on the config_dir param
     # passed to {#initialize}.
     #
-    # @return [Rutty::Config]
+    # @return [Rutty::Config] The loaded and parsed config object
     def config
       @config ||= Rutty::Config.load_config self.config_dir
     end
@@ -48,18 +51,43 @@ module Rutty
     # Lazy-load the {Rutty::Nodes} object containing the user-defined nodes' connection info.
     # Loads from the config_dir param passed to {#initialize}
     #
-    # @return [Rutty::Nodes]
+    # @return [Rutty::Nodes] The Nodes loaded from the config
     def nodes
       @nodes ||= Rutty::Nodes.load_config self.config_dir
     end
     
     ##
-    # If @config_dir is nil, returns {Rutty::Consts::CONF_DIR}. Otherwise return @config_dir.
+    # The user-specified config directory, falling back to the default on nil.
+    # Used by {Rutty::Nodes.load_config} and {Rutty::Config.load_config} to determine
+    # where to look for config files.
     #
     # @see Rutty::Consts::CONF_DIR
-    # @return [String] The user-specified config directory, falling back to the default on nil
+    # @return [String] The user-specified config directory, falling back to the default on nil.
     def config_dir
       (@config_dir.nil? && Rutty::Consts::CONF_DIR) || @config_dir
+    end
+    
+    ##
+    # The tool's output format, passed by the user via the rutty bin's flag. 
+    # Must be one of the elements of {Rutty::Consts::OUTPUT_FORMATS}.
+    # Defaults to {Rutty::Consts::DEFAULT_OUTPUT_FORMAT}
+    #
+    # @see Rutty::Consts::OUTPUT_FORMATS
+    # @see Rutty::Consts::DEFAULT_OUTPUT_FORMAT
+    # @return [String] The configured output format
+    def output_format
+      (@output_format.nil? && Rutty::Consts::DEFAULT_OUTPUT_FORMAT) || @output_format
+    end
+    
+    ##
+    # Sets @output_format, or raises an exception if the value is not included in {Rutty::Consts::OUTPUT_FORMATS}.
+    #
+    # @see Rutty::Consts::OUTPUT_FORMATS
+    # @raise [Rutty::InvalidOutputFormat] On a disallowed output format string
+    def output_format= format
+      raise Rutty::InvalidOutputFormat.new "Invalid output format: #{format}" unless Rutty::Consts::OUTPUT_FORMATS.include? format
+      
+      @output_format = format
     end
   end
 end

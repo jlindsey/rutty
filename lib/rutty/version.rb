e3fe910c95c277c44544d17b2d0281a50c30d561
Josh Lindsey <josh@cloudspace.com>
2010-11-13 11:21:35 -0500

    Version bump to 2.2.0

--- lib/rutty/version.rb
+++ lib/rutty/version.rb
@@ -8,8 +8,8 @@ module Rutty
   # @see http://semver.org
   module Version
     MAJOR = 2
-    MINOR = 1
-    PATCH = 3
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')

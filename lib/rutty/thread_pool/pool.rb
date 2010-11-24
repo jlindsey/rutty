8fd7ea3c445f03109e05c47ad5f7a6effb70b33e
Josh Lindsey <josh@cloudspace.com>
2010-11-24 13:41:45 -0500

    Document thread_pool classes

--- lib/rutty/thread_pool/pool.rb
+++ lib/rutty/thread_pool/pool.rb
@@ -3,9 +3,18 @@ require 'fastthread'
 require 'rutty/thread_pool/worker'
 
 module Rutty
+
+  ##
+  # A thread pool implementation using the work queue pattern. Shamelessly stolen from a StackOverflow question.
+  #
+  # @see http://stackoverflow.com/questions/81788/deadlock-in-threadpool/82777#82777
+  # @since 2.4.0
   class ThreadPool
+    ## @return [Integer] The maximum number of concurrent {Worker} threads
     attr_accessor :max_size
 
+    ##
+    # Creates a new {ThreadPool} instance with a maximum number of {Worker} threads.
     def initialize max_size = 10
       Thread.abort_on_exception = true
       
@@ -14,14 +23,25 @@ module Rutty
       @workers = []
     end
 
-    def size
+    ##
+    # The current size of the <tt>@workers</tt> array.
+    #
+    # @return [Integer] The current number of {Worker} threads
+    def current_size
       @workers.size
     end
 
+    ##
+    # Whether any {Worker} threads are currently executing, determined by 
+    # comparing the sizes of the <tt>@workers</tt> array and the <tt>@queue</tt>.
+    #
+    # @return [Boolean] Any currently executing workers?
     def busy?
-      @queue.size < @workers.size
+      @queue.size < current_size
     end
 
+    ##
+    # Loops over every {Worker} and calls {Worker#stop}, then clears the <tt>@workers</tt> array.
     def shutdown
       @workers.each { |w| w.stop }
       @workers = []
@@ -29,15 +49,26 @@ module Rutty
     
     alias :join :shutdown
 
-    def process block=nil,&blk
+    ##
+    # Gets a free worker and passes it the specified closure.
+    #
+    # @see #get_worker
+    # @param [Proc, Lambda, #call] block An instance of any class that responds to <tt>#call</tt>
+    def process block = nil, &blk
       block = blk if block_given?
       get_worker.set_block block
     end
 
     private
 
+    ##
+    # Gets a free {Worker}. If the {#max_size} has been reached and the <tt>@queue</tt> isn't empty,
+    # pops an existing {Worker} from there. Otherwise spins up a new {Worker} and adds it to the <tt>@workers</tt>
+    # array.
+    #
+    # @return [Rutty::Worker] A {Worker} ready for a new job
     def get_worker
-      if !@queue.empty? or @workers.size == @max_size
+      if !@queue.empty? or current_size == @max_size
         return @queue.pop
       else
         worker = Rutty::Worker.new @queue
--- lib/rutty/thread_pool/worker.rb
+++ lib/rutty/thread_pool/worker.rb
@@ -2,7 +2,20 @@ require 'thread'
 require 'fastthread'
 
 module Rutty
+  
+  ##
+  # The {ThreadPool} Thread wrapper, representing a single thread of execution in the pool.
+  #
+  # @since 2.4.0
   class Worker
+    
+    ##
+    # Initialize a new {Worker}. Sets up the initial state of this instance, then spins out its thread. The thread will
+    # wait until it's signaled (by being passed a new closure to execute, or being told to die, etc.), then resume execution
+    # of its task. The Thread is wrapped in a <tt>while @running</tt> loop, allowing for easy termination from {#shutdown}.
+    #
+    # @param [Queue] thread_queue The thread queue from the {ThreadPool} instance to add this worker to
+    # @return [Worker] The new, available Worker object.
     def initialize thread_queue
       @mutex = Mutex.new
       @cv = ConditionVariable.new
@@ -28,14 +41,27 @@ module Rutty
       end
     end
 
+    ##
+    # Returns the evaluation of <tt>@thread.inspect</tt>.
+    #
+    # @return [String] This thread's name
     def name
       @thread.inspect
     end
 
+    ##
+    # Returns the value of <tt>@block</tt>.
+    #
+    # @return [Proc, Lambda, #call] The <tt>@block</tt> object previously passed.
     def get_block
       @block
     end
 
+    ##
+    # Sets a new closure as the task for this worker. Signals the Thread to wakeup after assignment.
+    # 
+    # @param (see Rutty::ThreadPool#process)
+    # @raise [RuntimeError] If the thread is currently busy when called.
     def set_block block
       @mutex.synchronize do
         raise RuntimeError, "Thread already busy." if @block
@@ -44,15 +70,27 @@ module Rutty
         @cv.signal
       end
     end
-
+    
+    ##
+    # Sets the <tt>@block</tt> to <tt>nil</tt>. Called after the Thread has finished execution of a task to
+    # ready itself for the next one.
     def reset_block
       @block = nil
     end
 
+    ##
+    # Whether or not this Thread is currently executing. Determined by whether <tt>@block</tt> is <tt>nil</tt>.
+    #
+    # @return [Boolean] Is this Thread executing?
     def busy?
       @mutex.synchronize { !@block.nil? }
     end
 
+    ##
+    # Sets <tt>@running</tt> to <tt>false</tt>, causing the Thread to finish after the current loop. Signals
+    # the Thread to wake and resume, then calls <tt>#join</tt>.
+    #
+    # @see #initialize
     def stop
       @mutex.synchronize do
         @running = false
